c775617c63816de83cdcf257451a45b6dafb0bb7
harperim <harperim127@gmail.com>
2023-05-19 13:44:32 +0900

    임정인-swea-1984

--- swea/1986-지그재그숫자/임정인.py
+++ swea/1986-지그재그숫자/임정인.py
@@ -5,5 +5,11 @@ T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
+    answer = 0
+    for j in range(1, N+1):
+        if j % 2 == 0:
+            answer = answer - j
+        else:
+            answer = answer + j
 
-    print(tc, N)
+    print(f'#{tc} {answer}')
