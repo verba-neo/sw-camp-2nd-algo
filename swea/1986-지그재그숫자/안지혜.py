3b8baa1cf23263297fc77fffdc9c2466eb085df9
Jihye An <koanko.dev@gmail.com>
2023-05-19 13:43:46 +0900

    안지혜-swea-1986

--- swea/1986-지그재그숫자/안지혜.py
+++ swea/1986-지그재그숫자/안지혜.py
@@ -1,9 +1,17 @@
-import sys
-sys.stdin = open('input.txt')
+# import sys
+# sys.stdin = open('input.txt')
 
 T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
+    # print('N', N)
 
-    print(tc, N)
+    answer = 0
+    for i in range(1, N+1):
+        if i % 2 == 1:
+            answer += i
+        else:
+            answer -= i
+
+    print(f'#{tc} {answer}')
