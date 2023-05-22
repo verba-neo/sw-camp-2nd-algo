f2486fd3d884cfb2568aeb70fbb701297677764a
harperim <harperim127@gmail.com>
2023-05-22 10:59:32 +0900

    임정인-swea-L4834

--- swea/L4828-minmax/임정인.py
+++ swea/L4828-minmax/임정인.py
@@ -5,6 +5,7 @@ T = int(input())
 
 for tc in range(1, T+1):
     input()
+    # '띄어쓰기' 있을 때만 split
     i = list(map(int, input().split(" ")))
     answer = max(i) - min(i)
 
--- swea/L4834-숫자카드/임정인.py
+++ swea/L4834-숫자카드/임정인.py
@@ -6,7 +6,7 @@ T = int(input())
 
 for tc in range(1, T+1):
 
-    N = int(input())
+    N = int(input()) # 숫자 카드 개수
     num = input()
 
     count = [0]*10
@@ -16,10 +16,8 @@ for tc in range(1, T+1):
     maxx = 0
     idx = 0
     for j in range(len(count)):
-        if maxx <= count[j]:
+        if maxx <= count[j]: # 카드 장수가 같을 때는 더 큰 숫자로 갱신
             maxx = count[j]
             idx = j
 
     print(f'#{tc} {idx} {maxx}')
-
-
