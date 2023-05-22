297ef53a0747e6832706ba57e1288ea1abbf1c7d
thovy <98632452+thovy@users.noreply.github.com>
2023-05-22 15:10:27 +0900

    김도현-swea-1208

---
+++ swea/1208-flatten/김도현.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input.txt')
+
+for t in range(1, 11):
+    dump = int(input())
+    box_list = list(map(int, input().split()))
+
+    # 단순히 max min
+    highest = max(box_list)
+    lowest = min(box_list)
+
+    while dump:
+        highest = max(box_list)
+        lowest = min(box_list)
+
+        if highest - lowest < 2:
+            break
+
+        box_list[box_list.index(highest)] -= 1
+        box_list[box_list.index(lowest)] += 1
+        dump -= 1
+
+    # 다시 체크 안 하면 #6 에서 에러남.
+    highest = max(box_list)
+    lowest = min(box_list)
+
+    print(f'#{t} {highest - lowest}')
