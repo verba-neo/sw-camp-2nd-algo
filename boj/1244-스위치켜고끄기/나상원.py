5885a0d03ac0357ffe3647c40ba778121bfb2f96
Aphian <rsw7876@naver.com>
2023-05-24 17:14:00 +0900

    나상원-boj-1244

--- boj/1244-스위치켜고끄기/나상원.py
+++ boj/1244-스위치켜고끄기/나상원.py
@@ -2,14 +2,8 @@ import sys
 sys.stdin = open('./input.txt')
 # combinations, permutations
 
-def trans(number):
-    if switch_status[number] == 0:
-        switch_status[number] = 1
-    else:
-        switch_status[number] = 0
-    return
-
 N = int(input())
+
 switch_status = list(map(int, input().split()))
 students = int(input())
 
@@ -20,18 +14,23 @@ for tc in range(1, students+1):
             m_idx = 0
             if switch_idx % number == 0:
                 m_idx = switch_idx-1
-                trans(m_idx)
+                if switch_status[m_idx] == 0:
+                    switch_status[m_idx] = 1
+                else:
+                    switch_status[m_idx] = 0
 
     elif student_mf == 2:
-        trans(number)
-        for idx in range(N//2):
-            if number + idx > N or number - idx < 1:
-                break
-            if switch_status[number + idx] == switch_status[number - idx]:
-                trans(number + idx)
-                trans(number - idx)
+        if switch_status[:number - 1] == switch_status[(number - 1) * 2:number - 1:-1]:
+            for trans in range((number - 1) * 2 + 1):
+                if switch_status[trans] == 1:
+                    switch_status[trans] = 0
+                elif switch_status[trans] == 0:
+                    switch_status[trans] = 1
+        else:
+            if switch_status[number - 1] == 0:
+                switch_status[number - 1] = 1
             else:
-                break
+                switch_status[number - 1] = 0
 
 for no in range(N // 20 + 1):
     start = 20 * no
