be28236c03645e62063decd34eddd39af433acd9
Dev-nenda <jooeon1104@gamil.com>
2023-05-24 10:49:50 +0900

    장주언-swea-3143

---
+++ swea/3143-가장빠른문자열타이핑/장주언.py
@@ -0,0 +1,41 @@
+# 단어를 일반적으로 타이핑한다면 단어 길이만큼 타이핑 횟수
+# 특정 문자열 B가 저장되어 있다면 키를 한번 눌러서 타이핑 횟수를 줄일수 있다.
+# 첫번째 줄에 케이스의 수 T
+# 각 케이스마다 단어 A와 특정 문자열 B가 주어진다.
+# 1 <= A <= 10,000  1 <= B <= 100
+# A를 타이핑하기 위해 눌러야하는 최솟값을 출력
+
+import sys
+
+sys.stdin = open('./input.txt')
+
+T = int(input())
+
+for tc in range(1, T+1):
+
+    A, B = list(input().split())
+
+    idx = 0
+
+    count = 0
+
+    while True:
+        # A의 길이보다 idx가 길 경우 종료한다.
+        if idx >= len(A):
+
+            break
+
+        # A[idx] 부터 A[idx+len(B)]가 B이면 count += 1
+        elif A[idx: idx+len(B)] == B:
+            count += 1
+            idx += len(B)
+        # B가 아니면 count += 1, idx += 1
+        else:
+            count += 1
+            idx += 1
+
+    print(f'#{tc} {count}')
+
+
+
+
--- swea/5432-쇠막대기자르기/장주언.py
+++ swea/5432-쇠막대기자르기/장주언.py
@@ -8,7 +8,7 @@ T = int(input())
 
 for tc in range(1, T+1):
 
-    iron_rod = input()
+    iron_rod = list(map(str, input()))
 
     # 막대기의 개수
     rod = 0
@@ -20,15 +20,21 @@ for tc in range(1, T+1):
     idx = 0
 
     while True:
+        # iron_rod의 범위를 넘어가면 break
         if idx + 1 > len(iron_rod):
             break
-
+        # '(' 일 때 막대기의 갯수 추가
         elif iron_rod[idx] == '(':
             rod += 1
+        # ')' 일 때
         elif iron_rod[idx] == ')':
+            # 바로 직전에 '(' 여서 레이져였다면
             if iron_rod[idx-1] == '(':
+                # 막대기의 갯수를 하나 빼주고
                 rod -= 1
+                # 조각의 갯수에 막대기 갯수만큼 추가
                 count += rod
+            # 레이저가 아닌 ')'였다면, 막대의 갯수를 빼주고 조각을 하나 추가해준다.
             else:
                 rod -= 1
                 count += 1
