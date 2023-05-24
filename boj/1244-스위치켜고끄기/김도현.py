147997ed3b4cd1cd9d694c47dcd06f5e6be15bce
thovy <98632452+thovy@users.noreply.github.com>
2023-05-24 17:05:25 +0900

    김도현-boj-1244

--- boj/1244-스위치켜고끄기/김도현.py
+++ boj/1244-스위치켜고끄기/김도현.py
@@ -23,6 +23,11 @@ for s in range(students):
                 sw_dict[k] = change(v)
     else:
         i = 1
+        # 삼항연산자가 있습니다.
+        # sw_dict[number] = 0 if sw_dict[number] else 1
+        # 비트연산자?
+        # sw_dict[number] ^= 1
+        # 비트연산자 중 XOR(^)를 써서 X ^ 1 을 하면 X가 1이든 0이든 간에 반대로 바뀜.
         sw_dict[number] = change(sw_dict[number])
         while number + i <= len(sw) and number - i > 0:
             if sw_dict[number+i] == sw_dict[number-i]:
@@ -31,6 +36,8 @@ for s in range(students):
             else:
                 break
             i += 1
+            # i 를 증가시키는 게 if true 일 때로 옮기는 게 보기 좋지 않을까 생각.
+            # sw_dict[number] 를 생각 못했을 때, 적은 거임.
 
 
 answer = ' '.join(map(str, list(sw_dict.values())))
