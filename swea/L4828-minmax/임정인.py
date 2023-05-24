405daf760ab134ec40369f024dea1efe6bfb0649
harperim <harperim127@gmail.com>
2023-05-24 17:04:01 +0900

    임ã정인-swea-1210

--- swea/L4828-minmax/임정인.py
+++ swea/L4828-minmax/임정인.py
@@ -7,6 +7,7 @@ for tc in range(1, T+1):
     input()
     # '띄어쓰기' 있을 때만 split
     i = list(map(int, input().split(" ")))
+    # max, min 함수 쓰면 시간 복잡도 증가해서 비추천
     answer = max(i) - min(i)
 
     print(f'#{tc} {answer}')
