441684bada6c15f45da5580de424e9d8df1db046
thovy <98632452+thovy@users.noreply.github.com>
2023-05-24 17:52:00 +0900

    김도현-swea-1859

---
+++ swea/1859-백만장자프로젝트/김도현.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for t in range(1, T+1):
+
+    answer = 0
+    # days = int(input())
+    # total_price_list = list(map(int, input().split()))
+    # idx = 0
+    # while idx < len(total_price_list):
+    #     price_list = total_price_list[idx:]
+    #     # print(price_list)
+    #     highest_day = price_list.index(max(price_list))
+    #     print(idx)
+    #     if highest_day != 0:
+    #         for i in range(highest_day+1):
+    #             if price_list[i] < price_list[highest_day]:
+    #                 answer += price_list[highest_day] - price_list[i]
+    #             else:
+    #                 idx += 1
+    #                 break
+    #             idx = highest_day+1
+    #     else:
+    #         idx += 1
+    # ??? idx 조절을 해봅시다.
+
+
+    print(f'#{t} {answer}')
