788b1109575fcb39d453574e74ae4b111a06ee14
thovy <98632452+thovy@users.noreply.github.com>
2023-05-25 16:14:15 +0900

    김도현-swea-L4873

--- swea/L4866-괄호검사/김도현.py
+++ swea/L4866-괄호검사/김도현.py
@@ -34,3 +34,21 @@ for t in range(1, T+1):
     print(f'#{t} {answer}')
 
     # 스택으로 어케 푸나염
+    # stack 으로 사용하기 위해서
+    stack = []
+    answer = 1
+    for char in string:
+        if char == '(' or char == '[':
+            stack.append(char)
+        elif char == ')':
+            # stack 이 없다면 pop 을 못 합니다. error 가 발생하니 stack 의 유무를 먼저 확인해야 합니다.
+            if stack and '(' != stack.pop():
+                answer = 0
+                break
+        elif char == '}':
+            if stack and '{' != stack.pop():
+                answer = 0
+                break
+    # stack 에 남은 게 있다면
+    if stack:
+        answer = 0
