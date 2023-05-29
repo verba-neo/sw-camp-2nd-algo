9dd7dbdf8e15e58cc1c96acf972512fd961d9a7e
Jihye An <koanko.dev@gmail.com>
2023-05-29 23:18:29 +0900

    안지혜-boj-1260

--- boj/1260-dfs-bfs/안지혜.py
+++ boj/1260-dfs-bfs/안지혜.py
@@ -1,88 +1,105 @@
-import sys
-sys.stdin = open('input.txt')
-
-N, M, V = map(int, input().split())
-connection_dict = {}
-
-# 관계를 나타내는 딕셔너리 생성
-for _ in range(M):
-    v1, v2 = map(int, input().split())
-
-    if connection_dict.get(v1):
-        connection_dict[v1] = connection_dict[v1] + [v2]
-        connection_dict[v1].sort()
-    else:
-        connection_dict.setdefault(v1, [v2])
-        connection_dict[v1].sort()
-
-    if connection_dict.get(v2):
-        connection_dict[v2] = connection_dict[v2] + [v1]
-        connection_dict[v2].sort()
-    else:
-        connection_dict.setdefault(v2, [v1])
-        connection_dict[v2].sort()
-
-# 인접 접점 중, 방문 안 한 w 값 찾는 함수
-def search_w_unvisited(v, visited):
-    w = None
-    for el in connection_dict[v]:
-        if visited.get(el) != True:
-            w = el
-            break
-    return w
+# import sys
+# sys.stdin = open('input.txt')
+#
+# N, M, V = map(int, input().split())
+# connection_dict = {}
+#
+# # 관계를 나타내는 딕셔너리 생성
+# for _ in range(M):
+#     v1, v2 = map(int, input().split())
+#
+#     if connection_dict.get(v1):
+#         connection_dict[v1] = connection_dict[v1] + [v2]
+#     else:
+#         connection_dict.setdefault(v1, [v2])
+#     connection_dict[v1].sort()
+#
+#     if connection_dict.get(v2):
+#         connection_dict[v2] = connection_dict[v2] + [v1]
+#     else:
+#         connection_dict.setdefault(v2, [v1])
+#     connection_dict[v2].sort()
+#
+#
+# def dfs():
+#     visited = {}
+#     result = []
+#     stack = []
+#
+#     # V를 스택에 push
+#     stack.append(V)
+#
+#     while stack:
+#         v = stack.pop()
+#
+#         if not visited.get(v):
+#             visited.setdefault(v, True)
+#             result.append(v)
+#
+#             for i in connection_dict[v][::-1]:
+#                 stack += [i]
+#
+#     return result
+#
+#
+# def bfs():
+#     visited = {}
+#     result = []
+#     queue = []
+#
+#     queue.append(V)
+#
+#     while queue:
+#         v = queue.pop(0)
+#
+#         if not visited.get(v):
+#             visited.setdefault(v, True)
+#             result.append(v)
+#
+#         for i in connection_dict[v]:
+#             if not visited.get(i):
+#                 queue.append(i)
+#
+#     return result
 
 
 def dfs():
-    visited = {}
-    result = []
-    stack = []
+    visited = [False for _ in range(N+1)]
+    to_visits = [V]
+    # 방문 기록
+    history = []
 
-    # visited 에 1번째 v 를 True로 바꾸기, v를 스택에 push
-    result.append(V)
-    visited.setdefault(V, True)
-    stack.append(V)
-    v = V
-
-    while v:
-        # 방문 안 한 w 찾기
-        w = search_w_unvisited(v, visited)
-
-        # w 방문, w push, w를 v 로 바꿈.
-        while w:
-            result.append(w)
-            visited.setdefault(w, True)
-            stack.append(w)
-            v = w
-            w = search_w_unvisited(v, visited)
-
-        if stack:
-            v = stack.pop()
-        else:
-            break
-
-    return result
+    while to_visits:
+        current = to_visits.pop()
+        if not visited[current]:
+            visited[current] = True
+            history.append(current)
+            to_visits += sorted(graph[current], reverse=True)
+    return history
 
 
 def bfs():
-    visited = {}
-    result = []
-    queue = []
+    visited = [False for _ in range(N+1)]
+    to_visits = [V]
+    history = []
 
-    queue.append(V)
+    while to_visits:
+        current = to_visits.pop(0)
+        if not visited[current]:
+            visited[current] = True
+            history.append(current)
+            to_visits += sorted(graph[current])
+    return history
 
-    while queue:
-        v = queue.pop(0)
 
-        if not visited.get(v):
-            visited.setdefault(v, True)
-            result.append(v)
+N, M, V = map(int, input().split())
 
-        for i in connection_dict[v]:
-            if not visited.get(i):
-                queue.append(i)
+graph = [[] for _ in range(N+1)]
 
-    return result
-
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
 print(*dfs())
 print(*bfs())
