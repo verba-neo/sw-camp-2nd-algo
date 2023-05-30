c4ad82cabab3f38ec5db7cea891a4c47741a27bc
Sang <rsw7876@naver.com>
2023-05-30 16:15:43 +0900

    나상원-progrms-87946

---
+++ prgmrs/121684-체육대회/나상원.py
@@ -0,0 +1,8 @@
+def solution(ability):
+    answer = 0
+
+    return answer
+
+
+ability = [[40, 10, 10], [20, 5, 0], [30, 30, 30], [70, 0, 70], [100, 100, 100]]
+print(solution(ability))
--- prgmrs/92334-신고결과받기/나상원.py
+++ prgmrs/92334-신고결과받기/나상원.py
@@ -1,16 +1,22 @@
-def solution(id_list, report, k):
-
+def solution(id_li, repo, k):
+    
+    # set()으로 중복 제거/ 신고자 /신고 대상 split()으로 분리
     report_sp = []
     for report_cnt in set(report):
         report_sp.append(report_cnt.split())
     # print(report_sp)
-
+    
+    # id 별로 신고 처리 결과 '0'으로 초기화
     id_dic = {id_ck: 0 for id_ck in id_list}
+    
+    # 신고 대상을 key 값/ 신고자 는 다수가 있어야 하므로 value list 로 초기화 
     re_dic = {re_cnt: [] for re_cnt in id_list}
-
+    
+    # 신고 된 사람을 key 값으로 하는 dic 에 내용 추가
     for reporter, reported in report_sp:
         re_dic[reported].append(reporter)
 
+    # 신고자 가 k 값 이상 이면 처리 결과에 '1'을 더해줌
     for id_key in re_dic.keys():
         if len(re_dic[id_key]) >= k:
             for value in re_dic[id_key]:
