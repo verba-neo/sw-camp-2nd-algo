24295433b3704d3e0e8fb516882f4b55786968f5
Sang <rsw7876@naver.com>
2023-05-30 18:06:00 +0900

    나상원-progrms-121684

--- prgmrs/121684-체육대회/나상원.py
+++ prgmrs/121684-체육대회/나상원.py
@@ -1,6 +1,26 @@
 def solution(ability):
     answer = 0
 
+    students = len(ability)
+    items = len(ability[0])
+    attend_st = [False for _ in range(students)]
+
+    def dfs(items_idx, total):
+        nonlocal answer
+
+        if items_idx == items:
+            if total > answer:
+                answer = total
+            return
+
+        for students_idx in range(students):
+            if not attend_st[students_idx]:
+                attend_st[students_idx] = True
+
+                dfs(items_idx+1, total + ability[students_idx][items_idx])
+
+                attend_st[students_idx] = False
+    dfs(0, 0)
     return answer
 
 
