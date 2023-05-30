108db828b13af3f0236527c6662e35dd3161b19a
thovy <98632452+thovy@users.noreply.github.com>
2023-05-30 21:45:14 +0900

    김도현-prgmrs-87946

--- prgmrs/121684-체육대회/김도현.py
+++ prgmrs/121684-체육대회/김도현.py
@@ -18,14 +18,30 @@ def solution(ability):
 
 # 재귀로 풀어보기
 
-def my_permutation():
-    
-    return
-
 def solution2(ability):
     answer = 0
+    student_count = len(ability)
+    sport_count = len(ability[0])
+    visited = [False for _ in range(student_count)]
 
+    def my_permutation(sport_idx, total):
+        nonlocal answer
+        if sport_idx == sport_count:
+            if total > answer:
+                answer = total
+            return
 
+        for student_idx in range(student_count):
+            if not visited[student_idx]:
+                visited[student_idx] = True
+                print(sport_idx, total, ability[student_idx][sport_idx])
+                # 혹여나 sport 에 모두 채워넣어서(sport_idx == sport_count) answer 을 반환해야할 수도 있으니
+                # total 을 계산해서 보내줘야합니다. 거기서 answer 을 total 로 맞춰서 dfs 를 끝내면 answer 이 최신 값으로 새로고칩 됩니다.
+                my_permutation(sport_idx+1, total+ability[student_idx][sport_idx])
+                visited[student_idx] = False
+        return
+
+    my_permutation(0, 0)
 
     return answer
 
--- prgmrs/87946-피로도/김도현.py
+++ prgmrs/87946-피로도/김도현.py
@@ -28,6 +28,20 @@ def solution(k, dungeons):
     return max_go_in
 
 
+# def solution2(k, dungeons):
+#     answer = 0
+#
+#     dun_len = len(dungeons)
+#     visited = [False for _ in range(dun_len)]     # 던전 길이만큼. 각 던전의 방문을 조회해야하므로. 한 조합에서 재방문 X.
+#
+#     def my_permutation():
+#
+#
+#
+#     return answer
+
+
 input_k = 80
 input_dungeons = [[80, 20], [50, 40], [30, 10]]
 print(solution(input_k, input_dungeons))
+print(solution2(input_k, input_dungeons))
