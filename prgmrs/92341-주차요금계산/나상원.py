2b74a2b4bea0b8cdb78163539bffb27501e146ee
Sang <rsw7876@naver.com>
2023-05-30 13:15:38 +0900

    ã나상원-progrms-92341

--- prgmrs/92341-주차요금계산/나상원.py
+++ prgmrs/92341-주차요금계산/나상원.py
@@ -1,11 +1,48 @@
-def solution(fees, records):
+import math
+
+
+def change_time(time):
+    h, m = map(int, time.split(':'))
+
+    return 60*h + m
+
+
+def solution(f, re):
     answer = []
 
-    records_sp = []
-    for record in records:
-        records_sp.append(record.split())
-    print(records_sp)
+    recodes_dic = {}
+    to_fee = {}
 
+    for record in re:
+        time, c_number, io = record.split()
+
+        if c_number not in recodes_dic:
+            recodes_dic[c_number] = [[time, io]]
+        else:
+            recodes_dic[c_number].append([time, io])
+    # print(records_dic)
+
+    for recode in recodes_dic:
+        to_time = 0
+        fee = fees[1]
+
+        if len(recodes_dic[recode]) % 2 != 0:
+            recodes_dic[recode].append(['23:59', 'OUT'])
+
+        for io in recodes_dic[recode]:
+            if io[1] == 'IN':
+                to_time -= change_time(io[0])
+            else:
+                to_time += change_time(io[0])
+
+        if to_time > fees[0]:
+            fee += math.ceil((to_time - fees[0]) / fees[2]) * fees[3]
+
+        to_fee[recode] = fee
+    to_fee = sorted(to_fee.items())
+
+    for c, f in to_fee:
+        answer.append(f)
 
     return answer
 
