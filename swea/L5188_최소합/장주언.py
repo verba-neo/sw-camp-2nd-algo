2d96bcbc6e8c70c8697dff8f2bf85cca4937d52c
Dev-nenda <jooeon1104@gmail.com>
2023-06-02 15:36:27 +0900

    장주언-swea-L5189

--- swea/L5188_최소합/장주언.py
+++ swea/L5188_최소합/장주언.py
@@ -2,31 +2,52 @@ import sys
 
 sys.stdin = open('./input.txt')
 
+
+def search(row, col):
+    global answer, inspection
+    # answer가 inspection보다 작으면 끝
+    if answer < inspection:
+        return
+    # 도착지에 도달했을때 answer값 갱신
+    if row == N - 1 and col == N - 1:
+        answer = min(answer, inspection)
+
+    # 방향은 하 우
+    directions = [(1, 0), (0, 1)]
+    for d_row, d_col in directions:
+
+        nxt_row = row + d_row
+        nxt_col = col + d_col
+        # 범위안에 있을 경우 방문하지 않았다면
+        if 0 <= nxt_row < N and 0 <= nxt_col < N and not visited[nxt_row][nxt_col]:
+            # 방문처리
+            visited[nxt_row][nxt_col] = True
+            # inspection 값에 더해주고
+            inspection += area[nxt_row][nxt_col]
+            # 다시 돌린다
+            search(nxt_row, nxt_col)
+            # 돌리고 아니면 False 처리하고 나와서 다시 돌린다
+            visited[nxt_row][nxt_col] = False
+            # 더했던 값은 다시 빼주고
+            inspection -= area[nxt_row][nxt_col]
+
+
 T = int(input())
 
 for tc in range(1, T+1):
 
     N = int(input())
 
-    lst = [list(map(int, input().split())) for _ in range(N)]
+    area = [list(map(int, input().split())) for _ in range(N)]
 
-    row, col = 0
+    answer = 10 * (2*N-1)
 
-    _sum = 0
+    inspection = area[0][0]
 
-    visited = []
+    visited = [[False for _ in range(N)] for _ in range(N)]
 
+    search(0, 0)
 
-def dfs(idx, total):
-
-    # 오른쪽 혹은 아래
-    directions = [(1, 0), (0, 1)]
-    for d_row, d_col in directions:
-
-        nxt_row = row + d_row
-        nxt_col = col + d_col
-
-        visited.apend(lst[nxt_row][nxt_col])
-
+    print(f'#{tc} {answer}')
 
 
--- swea/L5189_전자카트/장주언.py
+++ swea/L5189_전자카트/장주언.py
@@ -4,16 +4,22 @@ sys.stdin = open('./input.txt')
 
 
 def dfs(current, start, total):
-    global answer
 
+    global answer
+    # 현재 위치가 끝이라면
     if current == N - 1:
+        # answer 값 갱신
         answer = min(answer, total + area[start][0])
         return
-
+    # 범위를 순회하면서
     for i in range(1, N):
+        # 방문하지 않았고 i가 start 값이 아닌 경우
         if visited[i] is False and start != i:
+            # 방문처리
             visited[i] = True
+            # current 값을 올려주고 다음 start에  i 갱신, total 값 갱신
             dfs(current +1, i,  total + area[start][i])
+            # 순회하다가 아니면 False처리후 다시 순회
             visited[i] = False
 
 
