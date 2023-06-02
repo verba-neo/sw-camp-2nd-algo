b104c348564cd6ee852b66f7be045b0dae7b8234
Sang <rsw7876@naver.com>
2023-06-02 15:04:47 +0900

    나상원-swea-ãL5189

--- prgmrs/86971-전력망을둘로나누기/나상원.py
+++ prgmrs/86971-전력망을둘로나누기/나상원.py
@@ -1,17 +1,33 @@
 def solution(n, wires):
-    answer = -1
+    answer = n
 
-    N = 9
-    W = wires
+    def dfs(s):
+        nonlocal count
+        visited[s] = True
+        count += 1
 
-    visited = [False for _ in range(N+1)]
+        for nxt_s in graph[s]:
+            if not visited[nxt_s]:
+                dfs(nxt_s)
 
+    graph = [[] for _ in range(n+1)]
+    for wire1, wire2 in wires:
+        graph[wire1].append(wire2)
+        graph[wire2].append(wire1)
 
+    for w1, w2 in wires:
+        count = 0
+        visited = [False for _ in range(n+1)]
+        visited[w2] = True
+        dfs(w1)
 
+        dif = abs(count - (n-count))
+
+        if answer > dif:
+            answer = dif
 
     return answer
 
 
-n = 9
-wires = [[1, 3], [2, 3], [3, 4], [4, 5], [4, 6], [4, 7], [7, 8], [7, 9]]
-print(solution(n, wires))
+# 3
+print(solution(9, [[1, 3], [2, 3], [3, 4], [4, 5], [4, 6], [4, 7], [7, 8], [7, 9]]))
